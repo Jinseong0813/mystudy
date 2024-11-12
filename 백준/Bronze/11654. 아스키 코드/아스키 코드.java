9795119e86ca4c0ecc140cc05becd382247c7d11
Jinseong0813 <wlstl258@gmail.com>
2024-11-12 22:27:27 +0900

    [Bronze V] Title: 아스키 코드, Time: 168 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.nextLine();
+        int sum = S.charAt(0);
+        System.out.println(sum);
+
+
+
+
+    }
+    }
