c0e0389a436bad9111cefc0c926c4b662f1dbc74
Jinseong0813 <wlstl258@gmail.com>
2025-02-25 14:43:31 +0900

    [Bronze III] Title: 나는 요리사다, Time: 188 ms, Memory: 18516 KB -BaekjoonHub

--- 백준/Bronze/2953. 나는 요리사다/나는 요리사다.java
+++ 백준/Bronze/2953. 나는 요리사다/나는 요리사다.java
@@ -6,14 +6,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int [][] arr =new int[5][4];
         int [] sum = new int [5];
+        int max = 0;
+        int dab = 0;
         for(int i=0; i<5; i++){
             for(int j=0; j<4; j++){
                 arr[i][j] = sc.nextInt();
                 sum[i] += arr[i][j];
             }
         }
-        int max = sum[0], dab=1;
-        for(int i=1; i<5; i++){
+        for(int i=0; i<5; i++){
             if(sum[i] > max){
                 max = sum[i];
                 dab = i+1;
